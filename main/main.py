a1354f3b30bdc38b327cc3cd56a320ed42c52752
GeorgijP <georgij.puzanov@inbox.ru>
2023-02-22 16:10:05 +0400

    Выполнено: Модифицируйте метод `__init__`, чтобы при создании экземпляра класса происходила инициализация следующих атрибутов класса:
    - id канала
    - название канала
    - описание канала
    - ссылка на канал
    - количество подписчиков
    - количество видео
    - общее количество просмотров
    
    Сделайте так, чтобы можно было получить id канала, но нельзя было его изменять/удалять.
    
    Реализуйте метод класса, который возвращает объект для работы с API ютуба. Используйте этот метод в классе, чтобы избежать дублирования кода.
    
    Реализуйте метод, который сохраняет информацию по каналу, хранящуюся в атрибутах экземпляра класса `Channel`, в json-файл. Продумайте самостоятельно формат записи в файл.

--- main/main.py
+++ main/main.py
@@ -2,4 +2,4 @@ from utils.utils import Youtube_anal
 
 item = Youtube_anal("UClI9aidW3X044NeB4QS-yxw", "YT_API_KEY")
 
-print(str(item.print_info()))
+item.info_save('Youtube_analitics.json')
--- utils/utils.py
+++ utils/utils.py
@@ -1,20 +1,36 @@
 # Импортируем нужные библиотеки
 import json
+
 from decouple import config
 from googleapiclient.discovery import build
 
+
 class Youtube_anal:
     """
-    Анализирует канал ютуба
+    Анализирует канал ютуб
     """
     def __init__(self, id_channel, api_key):
         self.api_key = config(api_key)
-        self.id_channel = id_channel
+        self.__id_channel = id_channel
+        self.channel_info = self.service().channels().list(id=id_channel, part='snippet, statistics').execute()
+        self.link = f"https://www.youtube.com/channel/{self.__id_channel}"
+        self.title = self.channel_info['items'][0]['snippet']['title']
+        self.description = self.channel_info['items'][0]['snippet']['description']
+        self.subscribers = self.channel_info['items'][0]['statistics']['subscriberCount']
+        self.video_count = self.channel_info['items'][0]['statistics']['videoCount']
+        self.view_count = self.channel_info['items'][0]['statistics']['viewCount']
+
+    def service(self):
         self.service = build('youtube', 'v3', developerKey=self.api_key)
+        return self.service
 
     def print_info(self):
         """
         Выводит в консоль информацию о канале.
         """
-        self.info = json.dumps(self.service.channels().list(id=self.id_channel, part='snippet, statistics').execute())
+        self.info = json.dumps(self.channel_info, indent=4)
         return self.info
+
+    def info_save(self, name_file):
+        with open(name_file, 'w', encoding='UTF-8') as file:
+            json.dump(self.channel_info, file, indent=4)
