596a46c4dd5d436e9dfe5f9ad5bd7071bf49772a
GeorgijP <georgij.puzanov@inbox.ru>
2023-03-04 16:51:40 +0400

    add class PlayList

--- utils/utils.py
+++ utils/utils.py
@@ -2,12 +2,16 @@
 import json
 from decouple import config
 from googleapiclient.discovery import build
+from abc import ABC, abstractmethod
+
+
+class MixinService(ABC):
+    pass
 
 
 class Youtube_anal:
-    """
-    Анализирует канал ютуб
-    """
+    """Анализирует канал ютуб"""
+
     def __init__(self, id_channel, api_key):
         self.api_key = config(api_key)
         self.__id_channel = id_channel
@@ -24,16 +28,12 @@ class Youtube_anal:
         return self.service
 
     def print_info(self):
-        """
-        Выводит в консоль информацию о канале.
-        """
+        """Выводит в консоль информацию о канале."""
         self.info = json.dumps(self.channel_info, indent=4)
         return self.info
 
     def info_save(self, name_file):
-        """
-        Сохраняет информацию о канале в файл
-        """
+        """Сохраняет информацию о канале в файл"""
         with open(name_file, 'w', encoding='UTF-8') as file:
             json.dump(self.channel_info, file, indent=4)
 
@@ -80,3 +80,24 @@ class PLVideo(Video):
 
     def __str__(self):
         return f"Название видео: {self.video_name} Название плейлиста: {self.playlist_name}"
+
+
+class PlayList:
+    """Обработка данных плейлиста"""
+    def __init__(self, id_playlist):
+        self.id_playlist = id_playlist
+        self.api_key = config("YT_API_KEY")
+        self.playlist_data = self.service().playlists().list(id=self.id_playlist, part='snippet, contentDetails').execute()
+        self.playlist_info = json.dumps(self.playlist_data, indent=4)
+        self.playlist_name = self.playlist_data['items'][0]['snippet']['title']
+
+        self.url = f"https://www.youtube.com/playlist?list={self.id_playlist}"
+
+    def service(self):
+        self.service = build('youtube', 'v3', developerKey=self.api_key)
+        return self.service
+
+
+pl = PlayList("PLguYHBi01DWr4bRWc4uaguASmo7lW4GCb")
+# print(pl.playlist_info)
+print(pl.url)
