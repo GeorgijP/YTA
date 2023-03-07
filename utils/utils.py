8283be5241c13e67bcffab08953eef5e8b14b9db
GeorgijP <georgij.puzanov@inbox.ru>
2023-03-07 19:39:45 +0400

    add class Mixin

--- utils/utils.py
+++ utils/utils.py
@@ -6,14 +6,23 @@ from abc import ABC, abstractmethod
 
 
 class MixinService(ABC):
-    pass
 
+    def __init__(self):
+        self.api_key = config("YT_API_KEY")
 
-class Youtube_anal:
+    def service(self):
+        self.service = build('youtube', 'v3', developerKey=self.api_key)
+        return self.service
+
+    @abstractmethod
+    def __repr__(self):
+        pass
+
+class Youtube_anal(MixinService):
     """Анализирует канал ютуб"""
 
-    def __init__(self, id_channel, api_key):
-        self.api_key = config(api_key)
+    def __init__(self, id_channel):
+        super().__init__()
         self.__id_channel = id_channel
         self.channel_info = self.service().channels().list(id=id_channel, part='snippet, statistics').execute()
         self.link = f"https://www.youtube.com/channel/{self.__id_channel}"
@@ -22,10 +31,6 @@ class Youtube_anal:
         self.subscribers = self.channel_info['items'][0]['statistics']['subscriberCount']
         self.video_count = self.channel_info['items'][0]['statistics']['videoCount']
         self.view_count = self.channel_info['items'][0]['statistics']['viewCount']
-
-    def service(self):
-        self.service = build('youtube', 'v3', developerKey=self.api_key)
-        return self.service
 
     def print_info(self):
         """Выводит в консоль информацию о канале."""
@@ -49,24 +54,26 @@ class Youtube_anal:
         else:
             return False
 
+    def __repr__(self):
+        return f"Youtube_anal({self.__id_channel}, {self.api_key})"
 
-class Video:
+
+class Video(MixinService):
     """Класс для обработки статистики видео"""
     def __init__(self, id_video):
+        super().__init__()
         self.id_video = id_video
-        self.api_key = config("YT_API_KEY")
         self.video_data = self.service().videos().list(id=self.id_video, part='snippet, statistics').execute()
         self.video_info = json.dumps(self.video_data, indent=4)
         self.video_name = self.video_data['items'][0]['snippet']['title']
         self.video_view_count = self.video_data['items'][0]['statistics']['viewCount']
         self.video_like_count = self.video_data['items'][0]['statistics']['likeCount']
 
-    def service(self):
-        self.service = build('youtube', 'v3', developerKey=self.api_key)
-        return self.service
-
     def __str__(self):
         return f"Название видео: {self.video_name}"
+
+    def __repr__(self):
+        return f"Video({self.id_video})"
 
 
 class PLVideo(Video):
@@ -81,21 +88,22 @@ class PLVideo(Video):
     def __str__(self):
         return f"Название видео: {self.video_name} Название плейлиста: {self.playlist_name}"
 
+    def __repr__(self):
+        return f"PLVideo({self.id_video}, {self.id_playlist})"
 
-class PlayList:
+
+class PlayList(MixinService):
     """Обработка данных плейлиста"""
     def __init__(self, id_playlist):
+        super().__init__()
         self.id_playlist = id_playlist
-        self.api_key = config("YT_API_KEY")
-        self.playlist_data = self.service().playlistItems().list(id=self.id_playlist, part='snippet, contentDetails').execute()
+        self.playlist_data = self.service().playlists().list(id=self.id_playlist, part='snippet, contentDetails').execute()
         self.playlist_info = json.dumps(self.playlist_data, indent=4)
         # self.playlist_name = self.playlist_data['items'][0]['snippet']['title']
-
         self.url = f"https://www.youtube.com/playlist?list={self.id_playlist}"
 
-    def service(self):
-        self.service = build('youtube', 'v3', developerKey=self.api_key)
-        return self.service
+    def __repr__(self):
+        return f"PlayList({self.id_playlist})"
 
 
 pl = PlayList("PLguYHBi01DWr4bRWc4uaguASmo7lW4GCb")
