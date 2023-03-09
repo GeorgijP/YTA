6bc9c450785789b2b163bceef8235b2e5cee3fc3
GeorgijP <122865026+GeorgijP@users.noreply.github.com>
2023-03-09 21:55:55 +0400

    Update main.py

--- main/main.py
+++ main/main.py
@@ -1,5 +1,1 @@
-from utils.utils import Youtube_anal
-
-item = Youtube_anal("UClI9aidW3X044NeB4QS-yxw", "YT_API_KEY")
-
-item.info_save('Youtube_analitics.json')
+from utils.utils import Youtube_anal, Video, PLVideo
