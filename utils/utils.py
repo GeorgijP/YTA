cd826f2fda80d30fe01a870007dc91c98477b837
GeorgijP <georgij.puzanov@inbox.ru>
2023-03-09 21:53:47 +0400

    add show_best_video and test

--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,7 @@
 # Импортируем нужные библиотеки
+import datetime
 import json
+import isodate
 from decouple import config
 from googleapiclient.discovery import build
 from abc import ABC, abstractmethod
@@ -97,16 +99,38 @@ class PlayList(MixinService):
     def __init__(self, id_playlist):
         super().__init__()
         self.id_playlist = id_playlist
-        self.playlist_data = self.service().playlists().list(id=self.id_playlist, part='snippet, contentDetails').execute()
+        self.playlist_data = self.service().playlists().list(id=self.id_playlist, part='snippet, contentDetails', maxResults=50).execute()
         self.playlist_info = json.dumps(self.playlist_data, indent=4)
-        # self.playlist_name = self.playlist_data['items'][0]['snippet']['title']
+        self.playlist_name = self.playlist_data['items'][0]['snippet']['title']
         self.url = f"https://www.youtube.com/playlist?list={self.id_playlist}"
         self.playlist_video = self.service.playlistItems().list(playlistId=self.id_playlist, part='contentDetails').execute()
         self.playlist_video_info = json.dumps(self.playlist_video, indent=4)
+        self.video_ids: list[str] = [video['contentDetails']['videoId'] for video in self.playlist_video['items']]
+        self.video_response = self.service.videos().list(part='contentDetails,statistics', id=','.join(self.video_ids)).execute()
+
+    @property
+    def total_duration(self):
+        duration = datetime.timedelta(0)
+
+        for video in self.video_response['items']:
+            # Длительности YouTube-видео представлены в ISO 8601 формате
+            iso_8601_duration = video['contentDetails']['duration']
+            duration += isodate.parse_duration(iso_8601_duration)
+
+        return duration
+
+    def show_best_video(self):
+
+        videos = {}
+        for i in range(len(self.video_ids)):
+            videos[int(self.video_response['items'][i]['statistics']['likeCount'])] = self.video_ids[i]
+
+        return f"https://www.youtube.com/watch?v={videos[max(videos)]}"
+
     def __repr__(self):
         return f"PlayList({self.id_playlist})"
 
 
 pl = PlayList("PLguYHBi01DWr4bRWc4uaguASmo7lW4GCb")
-print(pl.playlist_video_info)
-
+print(pl.show_best_video())
+print(type(pl.total_duration))
